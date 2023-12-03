91b7c8216d1001f2556d2d66359d45896204be53
Sota Hirose <za446131@gmail.com>
2023-12-04 02:32:29 +0900

    add

--- home/isucon/webapp/node/src/handlers/user-handler.ts
+++ home/isucon/webapp/node/src/handlers/user-handler.ts
@@ -50,7 +50,7 @@ export const getIconHandler = [
       await conn.commit().catch(throwErrorWith('failed to commit'))
       const hash = createHash('sha256').update(new Uint8Array(icon.image)).digest('hex')
       if (icon_hash && icon_hash.includes(hash)){
-         return new Response(null, { status: 304 })
+         return c.text("", 304)
       }
 
       return c.body(icon.image, 200, {
