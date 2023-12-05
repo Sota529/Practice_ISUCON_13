6d1a5ddff9773dfa42fadf2784828dfb74e3ab6a
Sota Hirose <za446131@gmail.com>
2023-12-05 23:25:15 +0900

    add

--- home/isucon/webapp/node/src/handlers/user-handler.ts
+++ home/isucon/webapp/node/src/handlers/user-handler.ts
@@ -51,7 +51,7 @@ export const getIconHandler = [
       const hash = createHash('sha256').update(new Uint8Array(icon.image)).digest('hex')
       console.log("icon_hash",icon_hash);
       console.log("hash",hash);
-      console.log("icon_hash.includes(hash)",icon_hash.includes(hash));
+      console.log("hasshashhash",icon_hash.includes(hash));
       
       if (icon_hash !== undefined && icon_hash.includes(hash)){
          return new Response(null, { status: 304 })
