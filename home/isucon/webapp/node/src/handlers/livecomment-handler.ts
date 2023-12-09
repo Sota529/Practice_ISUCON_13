42d7c9718044ca0d090bab4239f7f0b78b78c3b7
Sota Hirose <za446131@gmail.com>
2023-12-09 16:26:13 +0900

    fix

--- home/isucon/webapp/node/src/handlers/livecomment-handler.ts
+++ home/isucon/webapp/node/src/handlers/livecomment-handler.ts
@@ -356,7 +356,7 @@ export const moderateHandler = [
                 (SELECT CONCAT('%', ?, '%')	AS pattern) AS patterns
                 ON texts.text LIKE patterns.pattern) >= 1;
               `,
-              [livecomment.id, livecomment.comment, ngword.word],
+              [livecomment.id, livecomment.comment, ngword],
             )
             .catch(
               throwErrorWith(
